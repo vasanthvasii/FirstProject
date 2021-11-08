8be2877e447499cc268505378467ada9e0a700bd
vasanth <vasanthshetty775@gmail.com>
2021-11-08 16:59:40 +0530

    branch-2 changes

--- src/com/vasanth/Main.java
+++ src/com/vasanth/Main.java
@@ -8,5 +8,7 @@ public class Main {
         System.out.println("hai chikkamagaluru");
 
         System.out.println("changes doing in branch-1");
+
+        System.out.println("branch-2 changes");
     }
 }
